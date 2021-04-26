d1529db7420e8c7ea0f83cd1b68e6d86de36474c
981011512 <981011512@qq.com>
2021-04-27 07:02:36 +0800

    Update FileSystemService.java
    支持阿里云oss文件上传服务

--- cf-file/cf-file-interface/src/main/java/com/cf/file/service/FileSystemService.java
+++ cf-file/cf-file-interface/src/main/java/com/cf/file/service/FileSystemService.java
@@ -3,7 +3,7 @@ package com.cf.file.service;
 import com.cf.file.response.UploadFileResult;
 
 public interface FileSystemService {
-
+    //可轻松改造升级支持阿里云oss
     public UploadFileResult upload(byte[] fileByte , String originalFilename, String contentType,String filetag,String businesskey,String metadata);
 
 }
